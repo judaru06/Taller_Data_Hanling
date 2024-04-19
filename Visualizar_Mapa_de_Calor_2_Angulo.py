41d12d86e466ac1a0ad228a764287975e9f0fe20
judaru06 <juanrojas166633@correo.itm.edu.co>
2024-04-19 15:12:36 -0500

    Se realiza codigo para poder calcular los mapas de calor de cualquier arreglo de antenas Tx.. Se crean dos carpetas para guardar las iamgenes de los angulos 1 y 2.

--- Visualizar_Mapa_de_Calor_2_Angulo.py
+++ Visualizar_Mapa_de_Calor_2_Angulo.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import re
+
 def parse_complex_numbers_correctly(matrix_str):
     # Limpiar y preparar la cadena para el procesamiento
     matrix_str = matrix_str.replace('[', '').replace(']', '').replace('\n', '').replace(' ', '')
@@ -21,13 +22,20 @@ def parse_complex_numbers_correctly(matrix_str):
 
 
 # Cargar los datos
-data = pd.read_csv(r'D:\Maestria\Semestre_2\Programacion Cientifica\Taller_Data_Hanling\Datos\music_data_2_angles.csv')
-    
+data_1 = pd.read_csv(r'D:\Maestria\Semestre_2\Programacion Cientifica\Taller_Data_Hanling\Datos\music_data_1_angles.csv')
+data_2 = pd.read_csv(r'D:\Maestria\Semestre_2\Programacion Cientifica\Taller_Data_Hanling\Datos\music_data_2_angles.csv')
+
 # Aplicar la función de parseo
-data['parsed_matrices'] = data['Rx'].apply(parse_complex_numbers_correctly)
+data_1['parsed_matrices'] = data_1['Rx'].apply(parse_complex_numbers_correctly)
+data_2['parsed_matrices'] = data_2['Rx'].apply(parse_complex_numbers_correctly)
+
 # Extraer partes reales e imaginarias y guardarlas
-real_parts = np.array([m.real for m in data['parsed_matrices']])
-imaginary_parts = np.array([m.imag for m in data['parsed_matrices']])
+real_parts_1 = np.array([m.real for m in data_1['parsed_matrices']])
+imaginary_parts_1 = np.array([m.imag for m in data_1['parsed_matrices']])
+real_parts_2 = np.array([m.real for m in data_2['parsed_matrices']])
+imaginary_parts_2 = np.array([m.imag for m in data_2['parsed_matrices']])
+
+#Esto es por si lo quiero guardar con otros formatos
 #np.savetxt('D:\Maestria\Semestre_2\Programacion Cientifica\Taller_Data_Hanling\Imagenes\real_matrices.csv', real_parts.reshape(-1, 144), delimiter=',', header=','.join([f'R{i}' for i in range(1, 145)]))
 #np.savetxt('D:\Maestria\Semestre_2\Programacion Cientifica\Taller_Data_Hanling\Imagenes\imaginary_matrices.csv', imaginary_parts.reshape(-1, 144), delimiter=',', header=','.join([f'I{i}' for i in range(1, 145)]))
 
@@ -53,9 +61,10 @@ def plot_heatmaps(real_matrices, imag_matrices, num_figures, save_path):
         # Ajustar el layout para evitar solapamientos
         plt.tight_layout()
         # Guardar la figura
-        plt.savefig(f"{save_path}/heatmap_2_2_figure_{i+1}.png")
+        plt.savefig(f"{save_path}/heatmap_1_figure_{i+1}.png")
         plt.close()  # Cerrar la figura para liberar memoria
 
 
 # Llamada a la función para generar y guardar las figuras
-plot_heatmaps(real_parts, imaginary_parts, num_figures=4, save_path="D:\Maestria\Semestre_2\Programacion Cientifica\Taller_Data_Hanling\Imagenes")
+plot_heatmaps(real_parts_1, imaginary_parts_1, num_figures=4, save_path="D:\Maestria\Semestre_2\Programacion Cientifica\Taller_Data_Hanling\Imagenes\Angulo_1")
+plot_heatmaps(real_parts_2, imaginary_parts_2, num_figures=4, save_path="D:\Maestria\Semestre_2\Programacion Cientifica\Taller_Data_Hanling\Imagenes\Angulo_2")
